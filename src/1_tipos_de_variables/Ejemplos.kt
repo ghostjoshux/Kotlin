f054627f7f8193d756c43bd79307a00d508bcc69
ghostjoshux <joshuaquesada26@gmail.com>
2025-03-24 18:08:04 +0100

    Segundo commit

---
+++ src/1_tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,39 @@
+package `1_tipos_de_variables`
+
+fun main() {
+    //todas las variables val son inmutables
+
+    //Variable inmutable de tipo entero
+    val edad: Int
+    edad = 48
+    //Variable inmutable de tipo decimal
+    val sueldo:Float
+    sueldo=1200.55f //siempre lleva una "f" al final del valor
+    //Variable inmutable de tipo decimal
+    val total: Double
+    total = 70000.24
+    //Variable inmutable de tipo alfanumerico
+    val titulo: String
+    titulo = "Sistema de ventas"
+    //Variables mutables.las variables mutables comienzan con "var"
+    var mes: Int
+    mes = 1
+    //Algunas lineas màs de codigo
+    mes = 2
+    //Variable de tipo Char, almacena un unica caracter
+    var continua: Char = 's'
+    //Variable de tipo Boolean, true o false
+    var fin: Boolean = false
+
+    print("Joshua")
+    println("Quesada")
+
+    print(mes)
+
+
+
+
+
+
+
+}
---
+++ src/1_tipos_de_variables/Proyecto_2.kt
@@ -0,0 +1,18 @@
+package `1_tipos_de_variables`
+
+fun main(){
+
+    val variable1: Int
+    variable1 = 2
+    val variable2: Int
+    variable2 = 3
+    var suma: Int
+    suma = variable1 + variable2
+    println(suma)
+    var producto: Int
+    producto = variable1 * variable2
+    println(producto)
+
+
+
+}
